c8feb17ae4713a41d8fe79244d31195154e5c767
muratkarakus7 <muratkarakus7@gmail.com>
2013-10-19 00:39:33 +0800

    all rules are added, first offical release of project

---
+++ project-root/CandyV4-Release/src/interaction/GamePlayerInteraction.java
@@ -0,0 +1,80 @@
+package interaction;
+
+import java.util.Iterator;
+import java.util.List;
+
+import common.FedConfig;
+import rti.RTIAttribute;
+import rti.RTIInteraction;
+import rti.RTIObject;
+
+public class GamePlayerInteraction {
+
+    private int            candyAmount  = 0;
+    private int            objectHandle = 0;
+    private RTIInteraction rtiObject;
+
+    public GamePlayerInteraction() {
+        this.rtiObject = new RTIInteraction(FedConfig.getPlayerInteractionTag());
+        this.rtiObject.addNewAttribute(new RTIAttribute("candyAmount"));
+        this.rtiObject.addNewAttribute(new RTIAttribute("objectHandle"));
+    }
+
+    public void updateObjectAttributes() {
+        List<RTIAttribute> attributeList = this.rtiObject.getAttributeList();
+
+        for (Iterator<RTIAttribute> iterator = attributeList.iterator(); iterator.hasNext();) {
+            RTIAttribute rtiAttribute = (RTIAttribute) iterator.next();
+            if (rtiAttribute.getAttributeName().toLowerCase().equals("candyAmount".toLowerCase()) == true)
+                this.setCandyAmount(rtiAttribute.getAttributeValue());
+            
+            else if (rtiAttribute.getAttributeName().toLowerCase().equals("objectHandle".toLowerCase()) == true)
+                this.setObjectHandle(rtiAttribute.getAttributeValue());
+        }
+    }
+
+    public RTIInteraction updateRTIAttributes() {
+        List<RTIAttribute> attributeList = this.rtiObject.getAttributeList();
+
+        for (Iterator<RTIAttribute> iterator = attributeList.iterator(); iterator.hasNext();) {
+            RTIAttribute rtiAttribute = (RTIAttribute) iterator.next();
+            if (rtiAttribute.getAttributeName().toLowerCase().equals("candyAmount".toLowerCase()) == true)
+                rtiAttribute.setAttributeValue(this.getCandyAmount());
+            
+            if (rtiAttribute.getAttributeName().toLowerCase().equals("objectHandle".toLowerCase()) == true)
+                rtiAttribute.setAttributeValue(this.getObjectHandle());
+        }
+
+        return this.rtiObject;
+    }
+
+    public int getCandyAmount() {
+        return candyAmount;
+    }
+
+    public void setCandyAmount(int candyAmount) {
+        this.candyAmount = candyAmount;
+    }
+
+    public RTIInteraction getRtiObject() {
+        return rtiObject;
+    }
+
+    public void setRtiObject(RTIInteraction rtiObject) {
+        this.rtiObject = rtiObject;
+    }
+
+    public int getObjectHandle() {
+        return objectHandle;
+    }
+
+    public void setObjectHandle(int objectHandle) {
+        this.objectHandle = objectHandle;
+    }
+
+    @Override
+    public String toString() {
+        return "GamePlayerInteraction [candyAmount=" + candyAmount + ", rtiObject=" + rtiObject + "]";
+    }
+
+}
---
+++ project-root/CandyV4-Release/src/rti/RTIFedPropertiesBase.java
@@ -0,0 +1,39 @@
+package rti;
+
+public class RTIFedPropertiesBase {
+    private String fomFilePath;
+    private String fedSyncPoint;
+    private String federationName;
+
+    public RTIFedPropertiesBase(String fomFilePath, String fedSyncPoint, String federationName) {
+        super();
+        this.fomFilePath = fomFilePath;
+        this.fedSyncPoint = fedSyncPoint;
+        this.federationName = federationName;
+    }
+
+    public String getFomFilePath() {
+        return fomFilePath;
+    }
+
+    public void setFomFilePath(String fomFilePath) {
+        this.fomFilePath = fomFilePath;
+    }
+
+    public String getFedSyncPoint() {
+        return fedSyncPoint;
+    }
+
+    public void setFedSyncPoint(String fedSyncPoint) {
+        this.fedSyncPoint = fedSyncPoint;
+    }
+
+    public String getFederationName() {
+        return federationName;
+    }
+
+    public void setFederationName(String federationName) {
+        this.federationName = federationName;
+    }
+
+}
